c6c2c85d2a9a91160022a01371d0b7b5468745a4
SSH <12si47bun@gmail.com>
2023-12-11 22:53:58 +0900

    js,css 조정 - body에 커스텀효과 추가

--- source/lib.js
+++ source/lib.js
@@ -11,6 +11,7 @@ function aosEvent(){
 function menuEvent(){
   // 기본 변하지않는 변수
   const html = document.querySelector('html');
+  const body = document.querySelector('body');
   const header = document.querySelector('header');
 
   let menuWrap = document.querySelector('.h_main__wrap');
@@ -52,14 +53,19 @@ function menuEvent(){
   
     clickSubMenu();
 
+     // 기존 태그에 없던 속성추가
     function burgerMaker(event){
       if(event === undefined || event.getAttribute('aria-pressed') === "true"){
         header.setAttribute('data-header-mobile','off');
+        body.setAttribute('data-hamburger-effect','off');
+        
         burgerBtn.setAttribute('aria-pressed','false');
         hideSubMenu();
       } else {
         burgerBtn.setAttribute('aria-pressed','true');
         header.setAttribute('data-header-mobile','on');
+        body.setAttribute('data-hamburger-effect','on');
+
         linkToggle.forEach((link) => {
           link.addEventListener('click', function (event) {
             event.preventDefault();
